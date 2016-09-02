6fd0b672aca61ba6480f692990543bbe02172ba1
shimanekb <brandon.j.shimanek@gmail.com>
2016-09-02 01:36:11 +0000

    added first (unfinished) drill

---
+++ ch3/drill1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter you first name (followed by 'enter')\n";
+    string first_name;
+    cin >> first_name;
+    cout << "Hell, " << first_name << "!\n";
+
+    return 0;
+}
