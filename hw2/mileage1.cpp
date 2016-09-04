b3afd0c2656af7efd3d5be60239c1bfe5717194d
shimanekb <brandon.j.shimanek@gmail.com>
2016-09-04 15:53:25 -0500

    finished hw2 bonus

--- hw2/mileage1.cpp
+++ hw2/mileage1.cpp
@@ -7,26 +7,42 @@ class Mpg_log {
         Mpg_log(double);
         void buy_gas(double, double);
         double get_current_mpg(void);
+        double get_total_mpg(void);
     private:
+        double get_miles_traveled();
         double last_odometer;
         double this_odometer;
         double this_gas;
+        double total_miles;
+        double total_gas;
 };
 
 Mpg_log::Mpg_log (double starting_odometer) {
     this_odometer = starting_odometer;
     last_odometer = 0;
     this_gas = 0;
+    total_gas = 0;
+    total_miles = 0;
+}
+
+double Mpg_log :: get_miles_traveled() {
+    return this_odometer - last_odometer;
 }
 
 void Mpg_log :: buy_gas (double odometer, double gas) {
    last_odometer = this_odometer;
    this_odometer = odometer; 
    this_gas = gas;
+   total_miles += get_miles_traveled();
+   total_gas += gas;
 }
 
 double Mpg_log :: get_current_mpg() {
-    return ((this_odometer - last_odometer) / this_gas);
+    return (get_miles_traveled() / this_gas);
+}
+
+double Mpg_log :: get_total_mpg() {
+    return total_miles / total_gas;
 }
 
 int main() {
@@ -48,5 +64,6 @@ int main() {
         mpg_log.buy_gas(odometer, gas);
 
         cout << "This mpg: " << mpg_log.get_current_mpg() << "\n";
+        cout << "Total mpg: " << mpg_log.get_total_mpg() << "\n";
     }
 }
