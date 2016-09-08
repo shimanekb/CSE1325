a9948d126e2a3ad56cea57717eaca05a7292e33c
shimanekb <brandon.j.shimanek@gmail.com>
2016-09-08 18:53:48 -0500

    updated igore. added hw3 init

---
+++ hw3/elevator.cpp
@@ -0,0 +1,32 @@
+ #include "std_lib_facilities.h"
+ #include "elevator.h"
+
+ void Elevator::goto_floor(int floor) {
+    // Your code here
+ }
+
+ void Elevator::move() {
+   // Your code here
+ }
+
+ int Elevator::get_current_floor() {
+    // Your code here
+ }
+
+ int Elevator::get_desired_floor() {
+    // Your code here
+ }
+
+ bool Elevator::is_going_up() {
+    // Your code here
+ }
+
+ bool Elevator::has_arrived() {
+    // Your code here
+ }
+
+ bool Elevator::is_idle() {
+    // Your code here
+ }
+
+
