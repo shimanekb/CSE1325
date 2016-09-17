b4532bcee8b727aea650eb643ef5bb51b79b471e
shimanekb <brandon.j.shimanek@gmail.com>
2016-09-17 11:20:00 -0500

    created publication object

--- hw4/normal/publication.cpp
+++ hw4/normal/publication.cpp
@@ -1,20 +1,32 @@
 #include "publication.h"
 
-Publication::Publication(string isbn, string title, string author, int copyright_year): isbn(isbn), title(title)
-    , author(author), copyright_year(copyright_year) {}
+Publication::Publication(string isbn, string title, string author
+        , int copyright_year, Publication::Genre genre
+        , Publication::TARGET_AGE target_age)
+    : kIsbn(isbn), kTitle(title)
+    , kAuthor(author), kCopyrightYear(copyright_year)
+    , kGenre(genre), kTargetAge(target_age) {}
 
 string Publication::get_isbn() {
-    return isbn;
+    return kIsbn;
 }
 
 string Publication::get_title() {
-    return title;
+    return kTitle;
 }
 
 string Publication::get_author() {
-    return author;
+    return kAuthor;
 }
 
 int Publication::get_copyright_year() {
-    return copyright_year;
+    return kCopyrightYear;
+}
+
+Publication::Genre Publication::get_genre() {
+    return kGenre;
+}
+
+Publication::TARGET_AGE Publication::get_target_age() {
+    return kTargetAge;
 }
--- hw4/normal/publication.h
+++ hw4/normal/publication.h
@@ -7,16 +7,33 @@ using namespace std;
 
 class Publication {
     public:
-        Publication(string isbn, string title, string author, int copyright_year);
+        enum Genre {
+            FICTION,
+            NON_FICTION,
+            SELF_HELP,
+            PERFORMANCE
+        };
+        enum TARGET_AGE {
+            CHILDREN,
+            TEEN,
+            ADULT,
+            RESTRICTED 
+        };
+        Publication(string isbn, string title, string author, int copyright_year
+                , Publication::Genre genre, Publication::TARGET_AGE target_age);
         string get_isbn();
         string get_title();
         string get_author();
+        Genre get_genre();
         int get_copyright_year();
+        TARGET_AGE get_target_age();
     private:
-        const string isbn;
-        const string title;
-        const string author;
-        const int copyright_year;
+        const string kIsbn;
+        const string kTitle;
+        const string kAuthor;
+        const int kCopyrightYear;
+        const Publication::Genre kGenre;
+        const Publication::TARGET_AGE kTargetAge;
 };
 
 #endif
